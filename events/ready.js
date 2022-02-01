c666e719cc3d656d8e6fa6891780f5b528ca5a6b
ChinGH <86087456+chinghytb@users.noreply.github.com>
2022-02-01 13:11:24 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,7 @@
 const client = require("../index");
 
-client.on("ready", () =>
+client.on("ready", () => {
     console.log(`${client.user.tag} is online and ready to go!`)
-);
+    
+    client.user.setPresence({ activities: [{ name: 'discordjs v14'}], status: 'dnd' });
+)};
