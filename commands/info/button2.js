40d9e18187bcfab0394adbf5c74c3893c801fe59
ChinGH <86087456+chinghytb@users.noreply.github.com>
2022-02-03 11:53:25 +0800

    Create button2.js

---
+++ commands/info/button2.js
@@ -0,0 +1,20 @@
+const { ButtonComponent, ActionRow, EnumResolvers } = require('discord.js');
+const enumValues = EnumResolvers.resolveButtonStyle('DANGER');
+// You also can do this enum values with other button style.
+
+module.exports = {
+    name: 'button2',
+    description: 'descriptions here',
+
+    run: async (client, message, args) => {
+        const button = new ActionRow().addComponents(
+            new ButtonComponent({
+                label: 'test',
+                style: enumValues,
+                custom_id: '12345',
+            })
+        )
+
+        message.channel.send({ content: "version 14 button", components: [button] })
+    }
+}
